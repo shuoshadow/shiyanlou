e853e5f5ee4b5afd1f2d8a6bd961a22c73e1f27e
906486199 <906486199@qq.com>
2018-02-02 19:08:54 +0800

    tiaozhan3

--- calculator.py
+++ calculator.py
@@ -2,16 +2,18 @@
 
 import sys
 import os
+import csv
 
 class Args(object):
     def __init__(self):
         self.args = sys.argv[1:]
         if len(sys.argv) != 7:
-            print('Param Error')
+            print('Param1 Error')
             sys.exit(1)
         if not '-c' in sys.argv or not '-d' in sys.argv or not '-o' in sys.argv:
-            print('Param Error')
+            print('Param2 Error')
             sys.exit(1)
+
     def get_args(self):
         c_index = self.args.index('-c')
         configfile = self.args[c_index+1]
@@ -29,6 +31,7 @@ class Args(object):
 class Config(object):
     def __init__(self, configfile):
         self.config = self._read_config(configfile)
+
     def _read_config(self, configfilename):
         config = {}
         with open(configfilename) as file:
@@ -37,17 +40,20 @@ class Config(object):
                     sbname = line.split('=')[0].strip()
                     sbvalue = line.split('=')[1].strip()
                     config[sbname] = sbvalue
-                except Exception, e:
+                except Exception as e:
                     print(e)
                     sys.exit(1)
         return config
     def get_config(self, sbname):
-        return self.config[sbname]
+        config_data = self.config[sbname]
+        config_data = float(config_data)
+        return config_data 
 
 
 class UserData(object):
     def __init__(self, userdatafile):
-        self.userdata = self._read_user_data(userdatafile)
+        self.userdata = self._read_users_data(userdatafile)
+
     def _read_users_data(self, userdatafile):
         userdata = []
         with open(userdatafile) as file:
@@ -55,56 +61,81 @@ class UserData(object):
                 try:
                     gonghao = line.split(',')[0]            
                     gongzi = line.split(',')[1]
-                except Exception,e:
+                    gongzi = float(gongzi)
+                except Exception as e:
                     print(e)
                     sys.exit(1)
                 gz = (gonghao,gongzi)
                 userdata.append(gz)
         return userdata
+    #sbh shebaohe
+
+    def calculator(self, sbh, JiShuL, JiShuH):
+        userdata = self.userdata
+        gzlist = []
+        for user in userdata:
+            userid = user[0]
+            userwages = user[1]
+            jishu = userwages
+            if jishu < JiShuL:
+                jishu = JiShuL
+            if jishu > JiShuH:
+                jishu = JiShuH
+            shebao = jishu * sbh
+            geshui = self.geshui(jishu, shebao)
+            afwages = userwages - shebao - geshui
+            afwages = format(afwages, ".2f")
+            geshui = format(geshui, ".2f")
+            shebao = format(shebao, ".2f")
+            gzinfo = (userid, userwages, shebao, geshui, afwages)
+            gzlist.append(gzinfo)
+        return gzlist
+
+    def export(self, gzlist, gongzicsv):
+        with open(gongzicsv, 'w') as file:
+            writer = csv.writer(file, dialect='excel')
+            for line in gzlist:
+                #line = list(line)
+                #line = list(map(lambda x:[x],line))
+                #for info in line:
+                #    writer.writerows(info)
+                writer.writerow(line) 
+
+    def geshui(self, wages, shebao):
+        b_wages = wages - shebao - 3500
+        if b_wages <= 0:
+            taxes = 0
+        elif b_wages > 0 and b_wages <= 1500:
+            taxes = b_wages * 0.03 - 0
+        elif b_wages > 1500 and b_wages <= 4500:
+            taxes = b_wages * 0.1 - 105
+        elif b_wages > 4500 and b_wages <= 9000:
+            taxes = b_wages * 0.2 - 555
+        elif b_wages > 9000 and b_wages <= 35000:
+            taxes = b_wages * 0.25 - 1005
+        elif b_wages > 35000 and b_wages <= 55000:
+            taxes = b_wages * 0.3 - 2755
+        elif b_wages > 55000 and b_wages <= 80000:
+            taxes = b_wages * 0.35 - 5505
+        elif b_wages > 80000:
+            taxes = b_wages * 0.45 - 13505
+        return taxes
 
 
-class IncomeTaxCalculator(object):
-    def calc_for_all_userdata(self):
-        
-
-
-def shebao(wages):
-    shebao = wages * (0.08 + 0.02 + 0.005 + 0.06)
-    return shebao
-
-def geshui(wages):
-    b_wages = wages - shebao(wages) - 3500
-    if b_wages <= 0:
-        taxes = 0 
-    elif b_wages > 0 and b_wages <= 1500:
-        taxes = b_wages * 0.03 - 0
-    elif b_wages > 1500 and b_wages <= 4500:
-        taxes = b_wages * 0.1 - 105
-    elif b_wages > 4500 and b_wages <= 9000:
-        taxes = b_wages * 0.2 - 555
-    elif b_wages > 9000 and b_wages <= 35000:
-        taxes = b_wages * 0.25 - 1005
-    elif b_wages > 35000 and b_wages <= 55000:
-        taxes = b_wages * 0.3 - 2755
-    elif b_wages > 55000 and b_wages <= 80000:
-        taxes = b_wages * 0.35 - 5505
-    elif b_wages > 80000:
-        taxes = b_wages * 0.45 - 13505
-    return taxes
-
-yuangong = {}
-for wageinfo in sys.argv[1:]:
-    num = wageinfo.split(':')[0]
-    wages = wageinfo.split(':')[1]
-    try:
-        wages = int(wages)
-    except:
-        print("Parameter Error")
-        sys.exit(1)
-    af_wages = wages - shebao(wages) - geshui(wages)
-    af_wages = format(af_wages, ".2f")
-    yuangong[num] = af_wages
-
-for num, af_wages in yuangong.items():
-    re = num + ':' + af_wages
-    print(re)
+if __name__ == '__main__':
+    args = Args()
+    configfile,userdatafile,gongzicsv = args.get_args()
+    config = Config(configfile)
+    userdata = UserData(userdatafile)
+    JiShuL = config.get_config('JiShuL')
+    JiShuH = config.get_config('JiShuH')
+    YangLao = config.get_config('YangLao')
+    YiLiao = config.get_config('YiLiao')
+    ShiYe = config.get_config('ShiYe')
+    GongShang = config.get_config('GongShang')
+    ShengYu = config.get_config('ShengYu')
+    GongJiJin = config.get_config('GongJiJin')
+    sbh = YangLao + YiLiao + ShiYe + GongShang + ShengYu + GongJiJin
+    gzlist = userdata.calculator(sbh, JiShuL, JiShuH)
+    print(gzlist)
+    export = userdata.export(gzlist, gongzicsv)
